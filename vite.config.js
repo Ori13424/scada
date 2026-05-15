932e9f494a1a1491b805daaf2e2dcbd3f9ddc6c9
Fayem Muktadir Rahman <fayem13424drmc@gmail.com>
2026-05-15 23:00:25 +0600

    push

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+
+  // Inside vite.config.js
+define: {
+  global: 'window',
+}
 })
